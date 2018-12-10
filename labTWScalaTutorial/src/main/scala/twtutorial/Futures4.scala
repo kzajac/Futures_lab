583becc960fcb6c4c08a5dcddcec7a4eb61a3f1f
Katarzyna Rycerz <kzajac@agh.edu.pl>
2018-12-10 11:00:17 +0100

    zmiany

--- labTWScalaTutorial/src/main/scala/twtutorial/Futures4.scala
+++ labTWScalaTutorial/src/main/scala/twtutorial/Futures4.scala
@@ -7,38 +7,40 @@ import scala.util.{ Success, Failure }
 import scala.util.Random
 
 
-// Further reading:
-// 1. Utilities from Future companion object: https://www.scala-lang.org/api/current/scala/concurrent/Future$.html
-//    - E.g. 'Future.sequence' converts a collection of futures into one future for that collection 
-// 2. More about composing futures: http://rerun.me/2016/06/12/scala-futures-3-combinators-and-async/
-
 object Futures4 extends App {
+
 
   def asyncWork(n: Int): Future[Int] = Future {
     blocking { Thread.sleep(Random.nextInt(500)) }
-    println(n)
     n
   }
-
-  val result1 = asyncWork(1)
-  val result2 = asyncWork(2)
-  val result3 = asyncWork(3)
- 
-  // for comprehension is equivalent to
-  // result1.flatMap(r1 => result2.flatMap(r2 => result3.map(r3 => r1 + r2 + r3) ) )
+  // map
+  val stringF1 = asyncWork(3).map(n => n.toString)
   
-  val sum = for {
-    r1 <- result1
-    r2 <- result2
-    r3 <- result3
-  } yield (r1 + r2 + r3)
+   stringF1 onComplete {
+    case Success(value) => println(s"stringF1 = $value")
+    case Failure(e)     => e.printStackTrace
+  }
+  //map vs flatMap  
 
-  val sqr = sum map { s => s * s }
 
-  sqr onComplete {
-    case Success(value) => println(s"Result = $value")
+  //val stringF: Future[Future[String]]
+  val stringF = asyncWork(4).map(n => Future(n.toString))
+
+  stringF onComplete {
+    case Success(value) => println(s"stringF = $value")
+    case Failure(e)     => e.printStackTrace
+  }
+  //val flatStringF: Future[String]
+  val flatStringF = asyncWork(5).flatMap(n => Future(n.toString))
+
+   flatStringF onComplete {
+    case Success(value) => println(s"flatStringF = $value")
     case Failure(e)     => e.printStackTrace
   }
 
+ 
+  
+ 
   Thread.sleep(2000)
 }
